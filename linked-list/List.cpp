e4e98768ee4f6f9fa9eec4108fbfe74c67a705ac
BitSails <mrhthomp@gmail.com>
2019-03-20 11:29:04 -0400

    List ADT

---
+++ linked-list/List.cpp
@@ -0,0 +1,102 @@
+#include "List.h"
+
+
+class List::Node//inner (self-referential) class
+{
+  public:
+     int data = 0;
+     Node* link = nullptr;
+
+};//self-referential class
+
+
+void List::insertAt(int val, int k)
+{
+Node* newPtr = new Node{val};//Step 1
+
+
+if (k == 1) // special case 
+{
+  newPtr->link = frontPtr; //2a (special)
+  frontPtr = newPtr;// 2b (special)
+}
+else
+ {
+
+   Node* iPtr = frontPtr;//step 2a
+
+
+  //traverse list to (k-1)th node
+   for(int i = 0; i < k -1 ; i++)//step 2b
+     iPtr = iPtr->link;
+
+    newPtr->link = iPtr->link;//step 3a
+    iPtr->link = newPtr;//step 3b
+
+    }
+   
+   num_elements++;
+
+void List::removeAt(int k)
+{
+if( k == 1)//case 1
+{
+   Node* delPtr = frontPtr;
+   frontPtr = frontPtr->link;//point to 2nd node
+} 
+else
+{
+Node* iPtr = frontPtr;//step 2a
+
+//traverse list to (k-1)th node
+ for(int i = 0; i < k -1 ; i++)//step 2b
+   iPtr = iPtr->link;
+
+Node* delPtr = iPtr->link;//step 3a
+iPtr->link = delPtr->link; //steb 3b
+ }
+
+  delete delPtr;
+  num_elements--;
+
+
+
+}
+
+
+int getAt(int k)
+{
+
+
+}  
+
+
+int size()
+{
+
+}
+     
+
+void clear()
+{
+//OPTION 1
+Node* iPtr = frontPtr;
+while (iPtr != nullptr)
+{
+  frontPtr = frontPtr->link;
+  delete iPtr;
+  iPtr = frontPtr;
+} 
+
+
+}
+
+/*
+OPTION 2
+
+while (size() != 0)
+ removeAt(1);
+
+*/
+
+
---
+++ linked-list/List.h
@@ -0,0 +1,36 @@
+#ifndef LIST_H
+#define LIST_H
+// Implementing a List ADT (using a linked-list)
+class List
+{
+   public://operations
+    
+     //insert element at k-th position
+     void insertAt(int element, int k);
+      
+     //remove the k-th element
+     void removeAt(int k);
+
+     //view the k-th element
+     int getAt(int k);
+
+     //return number of elements 
+     int size();
+     
+     //make the ADT empty (again) 
+     void clear();
+
+
+
+     private:
+        class Node;//forward declaration 
+                   //(declared in List.cpp)
+        
+        Node* frontPtr = nullptr;
+        //initializing frontPtr here means we don't need        // to write a contstructor 
+
+        int num_elements = 0; //see above comment
+};
+
+
+#endif
